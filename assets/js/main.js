ff2b4de45ca935e94b35e2d164e96cb586366755
Federico <federico.scarpati1989@gmail.com>
2020-12-14 15:55:05 +0100

    Esercizio completato con il bonus
    Esercizio completato e inserimento del bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 //Crea una nuova istanza
 let app = new Vue({
-  //option object
   el: "#root",
   data: {
-  
+    sayHi:"Ciao e benvenuto ad Amalfi",
+    image: "https://inchiostrovirtuale.it/wp-content/uploads/2019/07/Amalfi-copertina.jpg"
   }
 });
